9b16d544bb311c8e05d2921de2393b11868e7bad
Arif Emre Karabıyık <arifemrekarabiyik1@gmail.com>
2026-05-16 12:43:14 +0300

    Update syscall
    İlk olarak ekrana çıktı alabilmek ve testlerin canlandığını görmek için exit ve write sistem çağrılarını hayata geçireceğiz.
    Pintos şu anda kullanıcı programlarının ekrana yazı yazmasını sağlayan write sistem çağrısını ve programların düzgünce kapanmasını sağlayan exit sistem çağrısını henüz barındırmıyor.

--- pintos/src/userprog/syscall.c
+++ pintos/src/userprog/syscall.c
@@ -3,8 +3,38 @@
 #include <syscall-nr.h>
 #include "threads/interrupt.h"
 #include "threads/thread.h"
+#include "threads/vaddr.h"
+#include "userprog/process.h"
 
 static void syscall_handler (struct intr_frame *);
+
+/* Programı sonlandıran ve ekrana hocanın istediği formatta log basan fonksiyon */
+void
+sys_exit (int status)
+{
+  struct thread *cur = thread_current ();
+  
+  /* Hocanın otomatik testlerinin (make check) aradığı ve PUAN VERDİĞİ kritik log formatı */
+  printf ("%s: exit(%d)\n", cur->name, status);
+  
+  /* Thread'i resmi olarak sonlandırıyoruz */
+  thread_exit ();
+}
+
+/* Ekrana (Konsola) yazı yazılmasını sağlayan fonksiyon */
+int
+sys_write (int fd, const void *buffer, unsigned size)
+{
+  /* fd == 1, standart çıktı (STDOUT - Konsol ekranı) demektir */
+  if (fd == 1)
+    {
+      /* Pintos kütüphanesindeki hazır putbuf fonksiyonunu kullanarak 
+         buffer içindeki veriyi tek seferde konsola basıyoruz */
+      putbuf (buffer, size);
+      return size; // Kaç bayt yazıldıysa onu döndürüyoruz
+    }
+  return -1;
+}
 
 void
 syscall_init (void) 
@@ -13,8 +43,33 @@ syscall_init (void)
 }
 
 static void
-syscall_handler (struct intr_frame *f UNUSED) 
+syscall_handler (struct intr_frame *f UNUSED)
 {
-  printf ("system call!\n");
-  thread_exit ();
+  /* Kullanıcı yığından (f->esp) sistem çağrısı numarasını alıyoruz */
+  int syscall_num = *(int *)f->esp;
+
+  switch (syscall_num)
+    {
+    case SYS_EXIT:
+      {
+        /* exit çağrısının 1 tane argümanı vardır (çıkış kodu). f->esp + 1 adresindedir. */
+        int status = *(int *)(f->esp + 4);
+        sys_exit (status);
+        break;
+      }
+    case SYS_WRITE:
+      {
+        /* write çağrısının 3 argümanı vardır: fd, buffer, size. f->esp + 1, +2, +3 sırasıyla dizilir. */
+        int fd = *(int *)(f->esp + 4);
+        const void *buffer = *(char **)(f->esp + 8);
+        unsigned size = *(unsigned *)(f->esp + 12);
+        
+        /* f->eax çekirdeğin kullanıcı programına döndüreceği cevap değeridir (Dönen bayt sayısı) */
+        f->eax = sys_write (fd, buffer, size);
+        break;
+      }
+    default:
+      printf ("Bilinmeyen sistem çağrısı: %d\n", syscall_num);
+      sys_exit (-1);
+    }
 }
