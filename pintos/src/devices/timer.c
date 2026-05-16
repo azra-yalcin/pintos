6ec1b636b2e3daf67af4e62326983065d6dd011a
Azra Yalçın <arenwashere@gmail.com>
2026-05-16 15:40:35 +0300

    Update timer.c

--- pintos/src/devices/timer.c
+++ pintos/src/devices/timer.c
@@ -24,6 +24,19 @@ static int64_t ticks;
    Initialized by timer_calibrate(). */
 static unsigned loops_per_tick;
 
+/* List of sleeping threads, ordered by wake_tick ascending. */
+static struct list sleep_list;
+
+/* Comparator: earlier wake_tick first. */
+static bool
+wake_tick_less (const struct list_elem *a,
+                const struct list_elem *b,
+                void *aux UNUSED)
+{
+  return list_entry (a, struct thread, sleep_elem)->wake_tick
+       < list_entry (b, struct thread, sleep_elem)->wake_tick;
+}
+
 static intr_handler_func timer_interrupt;
 static bool too_many_loops (unsigned loops);
 static void busy_wait (int64_t loops);
@@ -35,6 +48,7 @@ static void real_time_delay (int64_t num, int32_t denom);
 void
 timer_init (void) 
 {
+  list_init (&sleep_list);
   pit_configure_channel (0, 2, TIMER_FREQ);
   intr_register_ext (0x20, timer_interrupt, "8254 Timer");
 }
@@ -89,11 +103,19 @@ timer_elapsed (int64_t then)
 void
 timer_sleep (int64_t ticks) 
 {
-  int64_t start = timer_ticks ();
+  if (ticks <= 0)
+    return;
 
   ASSERT (intr_get_level () == INTR_ON);
-  while (timer_elapsed (start) < ticks) 
-    thread_yield ();
+
+  struct thread *cur = thread_current ();
+  enum intr_level old_level = intr_disable ();
+
+  cur->wake_tick = timer_ticks () + ticks;
+  list_insert_ordered (&sleep_list, &cur->sleep_elem, wake_tick_less, NULL);
+  thread_block ();
+
+  intr_set_level (old_level);
 }
 
 /* Sleeps for approximately MS milliseconds.  Interrupts must be
@@ -172,6 +194,17 @@ timer_interrupt (struct intr_frame *args UNUSED)
 {
   ticks++;
   thread_tick ();
+
+  /* Wake up any sleeping threads whose time has come. */
+  while (!list_empty (&sleep_list))
+    {
+      struct thread *t = list_entry (list_front (&sleep_list),
+                                     struct thread, sleep_elem);
+      if (t->wake_tick > ticks)
+        break;
+      list_pop_front (&sleep_list);
+      thread_unblock (t);
+    }
 }
 
 /* Returns true if LOOPS iterations waits for more than one timer
